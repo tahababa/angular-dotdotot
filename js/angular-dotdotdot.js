17edae16b1e3c0339fa4b809d3901648fd707be5
Taha Baba <taha.baba@live.com>
2015-03-24 13:09:57 +0200

    Update angular-dotdotdot.js

--- js/angular-dotdotdot.js
+++ js/angular-dotdotdot.js
@@ -1,11 +1,21 @@
 angular.module('dotdotdot-angular', [])
 	.directive('dotdotdot', function(){
-		return {
-			restrict: 'A',
-			link: function(scope, element, attributes) {
-				scope.$watch(function() {
-					element.dotdotdot({watch: true});
-				});
-			}
-		}
-	});
+        return {
+            restrict: 'A',
+            link: function(scope, element, attr) {
+                scope.$watch(function() {
+                    var option = {};
+                        option.watch = true;
+                    if( attr.dotdotdotUrl && attr.dotdotdotId ) {
+                        attr.dotdotdotEllipsis = (attr.dotdotdotEllipsis)?attr.dotdotdotEllipsis:'(...)';
+                        element.append(' <a class="readMore" href="' + attr.dotdotdotUrl + attr.dotdotdotId+ '"> '+attr.dotdotdotEllipsis+' </a> ');
+                        option.after = '.readMore';
+                        option.ellipsis = '';
+                    }
+                    else
+                        option.ellipsis	= (attr.dotdotdotEllipsis)? " "+attr.dotdotdotEllipsis : "";
+                    element.dotdotdot(option);
+                });
+            }
+        }
+    });
